67dcc8fc443365b0622913d587ea5254ccf126f6
Debarghya Majumdar <debarghy@amazon.com>
2018-07-12 17:56:21 +0530

    Added greetings when customer prints hi/hello etc

--- customerInputHandler.js
+++ customerInputHandler.js
@@ -21,77 +21,86 @@ var InventoryListing = [];
 
 const inventoryReader = require("./search.js");
 
-var handleCustomerMessage = function(customerId, custRequest, callback) {
+var greetings = ["hi", "hello", "hola", "hey"];
+var handleCustomerMessage = function(customerId, custRequestInput, callback) {
   //TODO: THIS IS HACK// Create crat in appropiate place.
-  cartHandler.createCart(customerId);
-
-  console.log("IN categorizeRequest" + customerId);
-  if (OPTIONS.includes(custRequest)) {
-    console.log("Execution of executeOption");
-    choiceHandler.executeOption(customerId, custRequest, function(
-      customerReply
-    ) {
-      callback(customerReply);
-    });
-    return;
-  }
-
-  var custRequest = custRequest.toLowerCase();
-  var terms = custRequest.split(" ");
-
-  for (value of addItemKeys) {
-    if (custRequest.includes(value)) {
-      console.log("Request is a addItemKeys");
-      return "ADD";
+  var custRequest = custRequestInput;
+  console.log("XXXXX ", custRequest, " YYYYYY");
+  cartHandler.getLatestCustomerCart(customerId, function(existingCart) {
+    if (existingCart == null || existingCart.status != "LIVE") {
+      cartHandler.createCart(customerId);
     }
-  }
-
-  for (value of removeItemKeys) {
-    if (custRequest.includes(value)) {
-      console.log("Request is a removeItemKeys");
-      return "REMOVE";
-    }
-  }
-
-  cartHandler.getLatestCustomerCart(customerId, function(cart) {
-    if (cart != null) {
-      var cartId = cart.cartId;
-      var numOfItems = Object.keys(cart.itemQuantityMap).length;
-      console.log("XXXXX numOfItems", numOfItems);
-    }
-  });
-
-  //Get current choice and see if it is live. If it is live execute the options.
-  var searchResult = inventoryReader.searchInventory(custRequest);
-  console.log(
-    "SearchResault is " +
-      util.inspect(searchResult, { showHidden: false, depth: null })
-  );
-
-  var addItemChoice = choiceHandler.createChoiceOnSearchSuggestion(
-    customerId,
-    searchResult,
-    function(addItemChoice) {
-      console.log(
-        "addItemChoice is " +
-          util.inspect(addItemChoice, { showHidden: false, depth: null })
+    console.log("XXXXX ", custRequest, " YYYYYY");
+    if (greetings.indexOf(custRequest.toLowerCase().trim()) > -1) {
+      callback(
+        "Hi, " +
+          customerId +
+          ", Welcome to Prime Now. What can I order for you today?"
       );
-      choiceHandler.persistChoice(addItemChoice, function(addItemChoice) {
-        var choiceMessage = choiceHandler.getDisplayChoice(addItemChoice);
-        console.log("choiceMessage is " + choiceMessage);
-        callback(choiceMessage);
+      return;
+    }
+
+    console.log("IN categorizeRequest" + customerId);
+
+    if (OPTIONS.includes(custRequest)) {
+      console.log("Execution of executeOption");
+      choiceHandler.executeOption(customerId, custRequest, function(
+        customerReply
+      ) {
+        callback(customerReply);
       });
+      return;
     }
-  );
 
-  removeItemKeys.forEach(function(value) {
-    if (custRequest.includes(value)) {
-      console.log("Request is a removeItemKeys");
-      return "REMOVE";
+    custRequest = custRequest.toLowerCase();
+    var terms = custRequest.split(" ");
+
+    for (value of addItemKeys) {
+      if (custRequest.includes(value)) {
+        console.log("Request is a addItemKeys");
+        return "ADD";
+      }
     }
+
+    for (value of removeItemKeys) {
+      if (custRequest.includes(value)) {
+        console.log("Request is a removeItemKeys");
+        return "REMOVE";
+      }
+    }
+
+    //Get current choice and see if it is live. If it is live execute the options.
+    var searchResult = inventoryReader.searchInventory(custRequest);
+    console.log(
+      "SearchResault is " +
+        util.inspect(searchResult, { showHidden: false, depth: null })
+    );
+
+    var addItemChoice = choiceHandler.createChoiceOnSearchSuggestion(
+      customerId,
+      searchResult,
+      function(addItemChoice) {
+        console.log(
+          "addItemChoice is " +
+            util.inspect(addItemChoice, { showHidden: false, depth: null })
+        );
+        choiceHandler.persistChoice(addItemChoice, function(addItemChoice) {
+          var choiceMessage = choiceHandler.getDisplayChoice(addItemChoice);
+          console.log("choiceMessage is " + choiceMessage);
+          callback(choiceMessage);
+        });
+      }
+    );
+
+    removeItemKeys.forEach(function(value) {
+      if (custRequest.includes(value)) {
+        console.log("Request is a removeItemKeys");
+        return "REMOVE";
+      }
+    });
+
+    return "DEFAULT";
   });
-
-  return "DEFAULT";
 };
 
 module.exports.handleCustomerMessage = handleCustomerMessage;
